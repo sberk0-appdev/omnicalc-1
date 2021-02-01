7da718af01e8c506e868817ae3e94f4d479ee4ce
sberk0 <sarah.berk@chicagobooth.edu>
2021-02-01 10:29:06 +0000

    Square root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,31 @@
 class ApplicationController < ActionController::Base
   def blank_square_form
-
     render({ :template => "calculation_templates/square_form.html.erb" })
   end
+
   def calculate_square_form
     # params = {"number"=>"42"}
-
     @num = params.fetch("number").to_f
-
     @square_of_num = @num ** 2
 
     render({ :template => "calculation_templates/square_results.html.erb" })
   end
-  def blank_root
 
+  def blank_root
     render({ :template => "calculation_templates/root_form.html.erb" })
   end
+
   def calculate_root
+    @num = params.fetch("user_number").to_f
+    @square_root_of_num = @num ** 0.5
 
     render({ :template => "calculation_templates/root_results.html.erb" })
   end
+
   def blank_random
     render({ :template => "calculation_templates/rand_form.html.erb" })
   end
+
   def calculate_random
     @lower = params.fetch("user_min").to_f
     @upper = params.fetch("user_max").to_f
